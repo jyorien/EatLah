a4ee8ff3e72f37bd56afb9b0d99cf70623b34522
joey <jyorin0206@gmail.com>
2020-12-11 15:43:38 +0800

    sorted comments

--- models/ReviewDB.js
+++ models/ReviewDB.js
@@ -5,7 +5,8 @@ const Review = require('../models/Review');
 
 class reviewDB {
     getReviews(request, respond) {
-        var sql = "SELECT review.*, user.username FROM review INNER JOIN user on review.user_id = user.user_id WHERE restaurant_id = ?"
+        // order by newest reviews
+        var sql = "SELECT review.*, user.username FROM review INNER JOIN user on review.user_id = user.user_id WHERE restaurant_id = ? ORDER BY review_id DESC"
         var id = request.params.id;
 
         db.query(sql, id, function(error, result) {
@@ -36,7 +37,7 @@ class reviewDB {
             }
         }
         function format_minute() {
-            if (minute > 0 && minute < 0) {
+            if (minute >= 0 && minute < 10) {
                 minute = `0${minute}`
             }
         }
@@ -59,34 +60,12 @@ class reviewDB {
         })
     }
     updateReview(request, respond) {
-        var now = new Date();
-        var hour = now.getHours() % 12;
-        var minute = now.getMinutes();
-        var date = now.getDate();
-        var month = now.getMonth();
-        var year = now.getFullYear()
-        var am_or_pm = ""
-        function format_am_or_pm() {
-            if (now.getHours() > 0 && now.getHours() < 12) {
-                am_or_pm = 'am'
-            }
-            else {
-                am_or_pm = 'pm'
-            }
-        }
-        function format_minute() {
-            if (minute > 0 && minute < 0) {
-                minute = `0${minute}`
-            }
-        }
-        format_am_or_pm();
-        format_minute();
-        var final_date = `${date}/${month}/${year} ${hour}:${minute} ${am_or_pm}`
-        var reviewObject = new Review(request.body.review_id, null, null, request.body.overall_rating, request.body.food_rating, request.body.service_rating, request.body.value_rating, request.body.review_title, request.body.comment,
-            request.body.will_recommend, final_date);
         
-        var sql = "UPDATE eatlah_reviews.review SET review_title = ?, comment = ?, overall_rating = ?, food_rating = ?, service_rating = ?, value_rating = ?, will_recommend = ?, date_posted = ? WHERE review_id = ?";
-        var values = [reviewObject.review_title, reviewObject.comment, reviewObject.overall_rating, reviewObject.food_rating, reviewObject.service_rating, reviewObject.value_rating, reviewObject.will_recommend, reviewObject.date_posted, reviewObject.review_id];
+        var reviewObject = new Review(request.body.review_id, null, null, request.body.overall_rating, request.body.food_rating, request.body.service_rating, request.body.value_rating, request.body.review_title, request.body.comment,
+            request.body.will_recommend, null);
+        
+        var sql = "UPDATE eatlah_reviews.review SET review_title = ?, comment = ?, overall_rating = ?, food_rating = ?, service_rating = ?, value_rating = ?, will_recommend = ? WHERE review_id = ?";
+        var values = [reviewObject.review_title, reviewObject.comment, reviewObject.overall_rating, reviewObject.food_rating, reviewObject.service_rating, reviewObject.value_rating, reviewObject.will_recommend, reviewObject.review_id];
         
         db.query(sql, values, function(error, result) {
             if (error) {
--- public/scripts/restaurant_display.js
+++ public/scripts/restaurant_display.js
@@ -1,4 +1,6 @@
 
+info_array=[]
+comments_count = 0;
 
 function retrieveInfo () {
     var name = sessionStorage.getItem('restaurant_name');
@@ -22,7 +24,6 @@ function retrieveInfo () {
     document.getElementById('res_max').innerHTML = max_price;
     document.getElementById('res_cuisine').innerHTML = cuisine;
 }
-
 function retrieveComments() {
     request = new XMLHttpRequest();
 
@@ -32,18 +33,23 @@ function retrieveComments() {
     request.open('GET', request_url, true);
     request.onload = function() {
         info_array = JSON.parse(request.responseText);
+        
         console.log(info_array);
+
         displayComments();
     }
     request.send();
 }
 
+// called when page is loaded
 function displayComments() {
     var section = document.getElementById('commentSection');
     section.innerHTML = "";
-    comments_count = info_array.length;
+    comments_count = 3;
 
     for (var i = 0; i < comments_count; i++) {
+        console.log(i)
+        console.log(info_array[i])
         var review_id = info_array[i].review_id;
         var review_title = info_array[i].review_title;
         var comment = info_array[i].comment;
@@ -57,10 +63,58 @@ function displayComments() {
         // TODO: format the comments 
         //var cell = ; 
 
-        section.insertAdjacentHTML('beforeend', cell); 
+        //section.insertAdjacentHTML('beforeend', cell); 
+
+    }
+}
+
+
+    // to be called by show more button
+    function displayAllComments() {
+        var section = document.getElementById('commentSection');
+        section.innerHTML = "";
+        comments_count = info_array.length;
+    
+        for (var i = 0; i < comments_count; i++) {
+            console.log(i)
+            console.log(info_array[i])
+            var review_id = info_array[i].review_id;
+            var review_title = info_array[i].review_title;
+            var comment = info_array[i].comment;
+            var date_posted = info_array[i].date_posted;
+            var overall_rating = info_array[i].overall_rating;
+            var food_rating = info_array[i].food_rating;
+            var service_rating = info_array[i].service_rating;
+            var value_rating = info_array[i].value_rating;
+            var username = info_array[i].username;
+    
+            // TODO: format the comments 
+            //var cell = ; 
+    
+            //section.insertAdjacentHTML('beforeend', cell); 
 
     }
 
-    
+    function sort_by(type) {
+        switch (type) {
+            case 'highest':
+                info_array.sort(function(a, b) {
+                    return b.overall_rating - a.overall_rating || b.food_rating - a.food_rating || b.service_rating - a.service_rating || b.value_rating || a.service_rating;
+                });
 
+            case 'lowest':
+                info_array.sort(function(a, b) {
+                    return a.overall_rating - b.overall_rating || a.food_rating - b.food_rating || a.service_rating - b.service_rating || a.value_rating || b.service_rating;
+                });
+
+            case 'newest':
+                info_array.sort(function(a, b) {
+                    return b.review_id - a.review_id;
+                });
+        }
+
+            
+        
+        
+    }
 }
