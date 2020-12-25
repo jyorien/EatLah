d25506ed9bd7550e4f3bd6d1130264ae355d4530
joey <jyorin0206@gmail.com>
2020-12-25 14:24:11 +0800

    made modal for sign up

--- public/scripts/login_signup.js
+++ public/scripts/login_signup.js
@@ -25,19 +25,23 @@ function login() {
     
 }
 
+function signUp() {
+    // FN,LN - 45, username - 16, pass - min 8, email - 128
+}
+
 function welcome() {
-    var urlParams = new URLSearchParams(window.location.search);
+
     var navElement;
     if ("user_name" in localStorage) {
 
         navElement = '<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="profileDrop" data-toggle="dropdown"> </a> <div class="dropdown-menu"><a class="dropdown-item" href="#">Link 1</a><a class="dropdown-item" href="#">Link 2</a> <a class="dropdown-item" onclick="localStorage.clear(); location.reload();" href="#">Logout</a> </div>'
         document.getElementById('topRightNavbar').insertAdjacentHTML("afterbegin",navElement); 
-        //var username = urlParams.getAll('username')
+
         document.getElementById('profileDrop').textContent = localStorage.getItem('user_name');
     }
     else {
         navElement = '<li class="nav-item"> <a class="nav-link" data-toggle="modal" data-target="#login_modal" href="#">Login </a> </li>'
-        navElement += '<li class="nav-item"> <a class="nav-link" href="#">Sign Up </a></li>'
+        navElement += '<li class="nav-item"> <a class="nav-link" data-toggle="modal" data-target="#sign_up_modal" href="#">Sign Up </a></li>'
         document.getElementById('topRightNavbar').insertAdjacentHTML("afterbegin",navElement); 
         
     }
